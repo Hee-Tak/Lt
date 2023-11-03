b925ed4291c1380113b946ca1b6bc72c276fa093
hitak <hitak77@naver.com>
2023-11-03 14:51:02 +0900

    로또 - 혼자 테스트중

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,16 +5,19 @@ fun main() {
     var user : List<Int>
     when((1..3).random()){
         1 -> {
+            println("<자동>")
             user = lotto.AutoLotto() //자동
         }
 
 
         2 -> {
+            println("<수동>")
             user = lotto.manualLotto() //수동
         }
 
 
         3 -> {
+            println("<반자동>")
             //반자동만들기
         }
 
