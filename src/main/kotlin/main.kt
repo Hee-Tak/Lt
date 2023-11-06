ade275a992b062db0d4d48919bfd84581db1aa77
hitak <hitak77@naver.com>
2023-11-06 17:41:15 +0900

    로또 - 혼자 테스트중

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,27 +3,42 @@ fun main() {
 
 
     var user : List<Int>
-    when((1..3).random()){
-        1 -> {
-            println("<자동>")
-            user = lotto.AutoLotto() //자동
-            lotto.printLotto(user)
+    println("1번 : 자동")
+    println("2번 : 수동")
+    println("3번 : 반자동")
+
+    var choose = readLine().toString().toInt()
+    while(choose in 1..3){
+        when(choose){
+            1 -> {
+                println("<자동>")
+                user = lotto.AutoLotto() //자동
+                lotto.printLotto(user)
+                break
+            }
+
+
+            2 -> {
+                println("<수동>")
+                user = lotto.manualLotto() //수동
+                lotto.printLotto(user)
+                break
+            }
+
+
+            3 -> {
+                println("<반자동>")
+                //반자동만들기
+                break
+            }
+
+            else -> {
+                choose = (1..3).random()
+            }
+
         }
-
-
-        2 -> {
-            println("<수동>")
-            user = lotto.manualLotto() //수동
-            lotto.printLotto(user)
-        }
-
-
-        3 -> {
-            println("<반자동>")
-            //반자동만들기
-        }
-
     }
+
 
     val result = lotto.prizeNumber()
 
