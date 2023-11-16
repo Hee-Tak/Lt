08667d22c80ba70dcbfc85f587fa7c795f53b188
hitak <hitak77@naver.com>
2023-11-17 02:49:10 +0900

    임시. 당첨번호순위쪽 조정중

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -114,8 +114,12 @@ public fun printSheets(sheet: MutableList<MutableList<Int>>) {
     }
 }
 
-public fun checkLotto(sheet: MutableList<MutableList<Int>>, result: MutableList<Int>) {
+public fun checkLotto(sheet: MutableList<MutableList<Int>>, prize: Pair<MutableList<Int>, Int>) {
+    val result = prize.first
+    val bonus = prize.second
+
     var num = 1
+
     for(list in sheet){
         var count = 1
         print("${num++}번) ")
@@ -126,16 +130,18 @@ public fun checkLotto(sheet: MutableList<MutableList<Int>>, result: MutableList<
             }
         }
         println("\t\t${count}개 일치")
+        if(count == 6){
+            println("1등")
+        } else if(count == 5){
+
+        }
+
     }
 }
 
 
-/*
-public fun rankLotto(){
 
-}
 
-*/
 //1등 : 6개 번호 모두 일치 (당첨확률 : 1 / 8,145,060 || 조합 개수 1) (기대 당첨금 1,952,160,000) (총 당첨금 중 4등과 5등 금액을 제외한 금액의 75%)
 //2등 : 5개 번호 일치 + 나머지 1개가 2등 보너스볼 번호 일치 (당첨확률 : 1 / 1,357,510 || 조합 개수 6) (기대 당첨금 54,226,666) (총 당첨금 중 4등과 5등 금액을 제외한 금액의 12.5%)
 //3등 : 5개 번호 일치 (당첨확률 : 1 / 35,724 || 조합 개수 228) (기대 당첨금 1,427,017) (총 당첨금 중 4등과 5등 금액을 제외한 금액의 12.5%)
