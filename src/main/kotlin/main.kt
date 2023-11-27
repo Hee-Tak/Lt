0e2ddf4d20256a1887e9795a5ca801a848c95bc3
hitak <hitak77@naver.com>
2023-11-27 17:50:11 +0900

    안드로이드앱으로 토이프로젝트로 해볼예정 +DB 연결은 한거같은데 실용적으로 쓰려면 방향성 수정 필요

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,7 +8,7 @@ fun main() {
     oneSet().oneSet()   //oneSet 클래스의 oneSet 함수 호출(원래는 생성자 불러내는 느낌으로 하려고 했는데..)
                         //원래는 val start = oneSet().oneSet() 이였는데, 이게 반환값이 결국에 void 다 보니까 (없다보니까) 그냥 이렇게해도 됨
                         //사실 자꾸 회색글씨로 표시되길래 알아차림
-
+    JDBC_TEST()
 }
 
 
@@ -17,8 +17,11 @@ fun main() {
 fun JDBC_TEST() {
     //JDBC 연결 정보
     val jdbcURL = "jdbc:mysql://localhost:3306/mysql"
-    val user = "your_username"
-    val password ="your_password"
+    //val user = "your_username"
+    //val password ="your_password"
+
+    val user = "root"
+    val password = "0000"
 
     //JDBC 드라이버 로드
     Class.forName("com.mysql.cj.jdbc.Driver")
